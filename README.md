314d323818a453862b82876d14d0b503412dc885
Sankar <iamkarnas.d@gmail.com>
2025-06-28 10:20:02 +0530

    created the webapp and pushing the raect files into repo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # FirstWebApp
+
+This is the Solar system app. This app was built using AI. App is inside the first-web-app folder. 
+
+I have to implement the deployment process as below 
+
+1. App will be deployed on VM and made accessable using firewall rules and update here in README.md
+
---
+++ first-web-app/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="solar-system">
+        <div className="sun"></div>
+        <div className="orbit orbit-1">
+          <div className="planet planet-1"></div>
+        </div>
+        <div className="orbit orbit-2">
+          <div className="planet planet-2"></div>
+        </div>
+        <div className="orbit orbit-3">
+          <div className="planet planet-3"></div>
+        </div>
+        <div className="orbit orbit-4">
+          <div className="planet planet-4"></div>
+        </div>
+        <div className="orbit orbit-5">
+          <div className="planet planet-5"></div>
+        </div>
+        <div className="orbit orbit-6">
+          <div className="planet planet-6"></div>
+        </div>
+        <div className="orbit orbit-7">
+          <div className="planet planet-7"></div>
+        </div>
+        <div className="orbit orbit-8">
+          <div className="planet planet-8"></div>
+        </div>
+      </div>
+      <div className="stars"></div>
+    </div>
+  );
+}
+
+export default App;
